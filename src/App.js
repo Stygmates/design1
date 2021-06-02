bc3c0a6dae63520de2ac8146a34c1f195d14603b
Stygmates <styg.tan.dat@gmail.com>
2021-06-02 07:38:18 +0200

    Add alt to images

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
       <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&display=swap" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&family=Lexend+Deca&display=swap" rel="stylesheet"/>        <div id="general">
         <div id="sedan" class="section">
-          <img src={sedan} class="image"/>
+          <img src={sedan} class="image" alt="Sedan"/>
           <h1>SEDANS</h1>
           Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.
           <button class="button">Learn more</button>
         </div>
         <div id="suv" class="section">
-        <img src={suv} class="image"/>
+        <img src={suv} class="image" alt="SUV"/>
         <h1>SUVS</h1>
           Take an SUV for its spacious interior, power and versatility. Perfect for your next family vacation and off-road adventures.
           <button class="button">Learn more</button>
           </div>
         <div id="cruise" class="section">
-        <img src={luxury} class="image"/>
+        <img src={luxury} class="image" alt="Luxury"/>
         <h1>LUXURY</h1>
           Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.
           <button class="button">Learn more</button>
